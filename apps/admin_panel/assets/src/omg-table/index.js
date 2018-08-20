11e82b7894163a54994fd18d64f23929fda29989
jarindr <jarindr23@gmail.com>
2018-08-20 18:13:53 +0700

    rename icon

--- apps/admin_panel/assets/src/omg-table/index.js
+++ apps/admin_panel/assets/src/omg-table/index.js
@@ -14,13 +14,6 @@ export const ThContent = styled.div`
   font-weight: 600;
   color: ${props => (props.active ? props.theme.colors.B400 : props.theme.colors.B100)};
 `
-const LoadMoreButton = styled(Button)`
-  padding-top: 5px;
-  padding-bottom: 5px;
-  i {
-    margin-right: 5px;
-  }
-`
 const TableContainer = styled.div`
   width: 100%;
   table {
@@ -239,19 +232,6 @@ class SortableTable extends PureComponent {
           .value()
       : this.props.rows
     return result
-  }
-  renderLoadMore = () => {
-    return (
-      <LoadMoreButton
-        styleType='secondary'
-        onClick={this.props.onClickLoadMore}
-        disabled={this.props.isLastPage}
-        size='small'
-      >
-        <Icon name='Chevron-Down' />
-        <span>Load More...</span>
-      </LoadMoreButton>
-    )
   }
   render () {
     return (
